9e78d9178c15245d99cb64b4e8125a3b4091bba6
AudreyNava <audrey.nava@kalypso.com>
2022-09-01 16:58:38 -0500

    App.js jalando

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const filesystem = require('fs');
-filesystem.writeFileSync('hola.txt', 'Hola desde Node');
+ const filesystem = require('fs');
+ filesystem.writeFileSync('hola.txt', 'Hola desde Node');
+
+ console.log("hola desde NodeConsole");
+
+ const arreglo = [500, 60, 90, 60, 80];
+
+ for(let item of arreglo){
+     console.log(item);
+ }
+
+ for(let item in arreglo){
+     setTimeout(() => {
+        console.log(item);
+     }, 5000);
+}
+
+ const http = require('http');
+
+const server = http.createServer((request, responce) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<!DOCTYPE html>');
+    response.write("<h1>Hola mundo!</h1>");
+    response.end();
+ })
+
+ server.listen(3000);
