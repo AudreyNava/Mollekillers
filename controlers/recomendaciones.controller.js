0aaad17b95f31e03e73237c341bb97970678d1c7
AudreyNava <69495191+AudreyNava@users.noreply.github.com>
2022-10-18 22:22:58 -0500

    ya vive a gusto la pagina

--- controlers/recomendaciones.controller.js
+++ controlers/recomendaciones.controller.js
@@ -7,8 +7,10 @@ exports.get_new = (request, response, next) => {
 }
 
 exports.post_new = (request, response, next) => {
-    const recomendacion = new Recomendacion(request.body.nombre, request.body.descripcion);
+    const recomendacion = new Recomendacion(request.body.nombre, request.body.descripcion, request.body.img);
     recomendacion.save(); //Manda la respuesta
+    console.log(recomendacion);
+    response.redirect('/recomendaciones/');
 }
 
 exports.get_root = (request, response, next) => {
